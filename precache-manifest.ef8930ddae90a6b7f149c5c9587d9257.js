fc2640edafd9b0a3c046040db3c51329903974cf
Adriana Gomez <gomezhinojosa.adriana@gmail.com>
2020-09-27 20:03:41 -0500

    Updates

---
+++ precache-manifest.ef8930ddae90a6b7f149c5c9587d9257.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3b82c5177ba50962c90e50987aad75",
+    "url": "/platzi-badges/index.html"
+  },
+  {
+    "revision": "d49568c915e892146932",
+    "url": "/platzi-badges/static/css/2.6cddb685.chunk.css"
+  },
+  {
+    "revision": "5737f98ed0d55c01adb1",
+    "url": "/platzi-badges/static/css/main.3c1308ec.chunk.css"
+  },
+  {
+    "revision": "d49568c915e892146932",
+    "url": "/platzi-badges/static/js/2.0729e55a.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/platzi-badges/static/js/2.0729e55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5737f98ed0d55c01adb1",
+    "url": "/platzi-badges/static/js/main.3e293567.chunk.js"
+  },
+  {
+    "revision": "60ff945f6066fe8d98da",
+    "url": "/platzi-badges/static/js/runtime-main.a9177a99.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "614b141a636bc2536f59793a823472ac",
+    "url": "/platzi-badges/static/media/error.614b141a.png"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges/static/media/stars.1d17360a.svg"
+  }
+]);
